4ecef2b1fc5addc4c59f0d5d4da49e85017ed085
toshpulatov00701 <toshpulatov00701@gmail.com>
2025-09-23 17:04:10 +0500

    add conf.py file 2

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,32 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'myAI'
 copyright = '2025, Abduhalil'
 author = 'Abduhalil'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    # 'sphinx.ext.autodoc',     # Agar docstringlardan API hosil qilmoqchi bo‘lsangiz
+    # 'sphinx.ext.napoleon',    # Google/NumPy style docstringlar uchun
+    # 'myst_parser',            # Agar Markdown ishlatmoqchi bo‘lsangiz
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'sphinx_rtd_theme'   # Read the Docs rasmi temasi
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# Maxsus logo (rasmni docs/_static/ ichiga joylashtiring)
+# html_logo = "_static/mylogo.png"
+
+# Ixtiyoriy sozlamalar (navigatsiya uchun qulaylik)
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 3,
+}
