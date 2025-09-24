c2cb15f17cb961b6ace0a7235c65ae44cd529d37
toshpulatov00701 <toshpulatov00701@gmail.com>
2025-09-24 13:59:25 +0500

    test

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ html_theme = 'sphinx_rtd_theme'   # Read the Docs rasmi temasi
 
 html_static_path = ['_static']
 html_css_files = ['custom.css']
+html_show_sourcelink = False
 
 # Maxsus logo (rasmni docs/_static/ ichiga joylashtiring)
 # html_logo = "_static/mylogo.png"
